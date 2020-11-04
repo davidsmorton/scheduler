92ae12b9f88b25f466cdd9fe5b420854137ef465
Scott Morton <smorton@Scotts-MacBook-Pro.local>
2020-11-03 17:05:17 -0700

    intergration testing application.js

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -2,7 +2,9 @@ import React from "react";
 import {render} from "@testing-library/react"
 import Application from "components/Application"
 
-describe("Appointment", () => { it("renders without crasing", () => {
+describe("Appointment", () => { 
+  
+  it("renders without crasing", () => {
   render(<Application />);
   });
 }); 
